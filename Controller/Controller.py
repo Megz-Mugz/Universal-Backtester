8d2e26c29fd29e3b950383b893a57b056ac93fdd
megz-mugz <69176595+megz-mugz@users.noreply.github.com>
2021-06-28 08:30:02 -0400

    Add files via upload

---
+++ Controller/Controller.py
@@ -0,0 +1,12 @@
+from Model.UserInputs import UserInputs
+from Model.Strategies import Strategies
+from Model.TradingStatistics import TradingStatistics
+from Model.SelectStrategy import SelectStrategy
+
+UserInputs
+Strategies.resample()
+SelectStrategy.execute()
+TradingStatistics.success()
+TradingStatistics.roi()
+TradingStatistics.printers()
+TradingStatistics.equityCurve()
